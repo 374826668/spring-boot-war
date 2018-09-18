bc715523459b7fe51df439b56b2c24dc99ae0feb
374826668 <mrlino1@163.com>
2018-09-18 20:47:00 +0800

    Initial commit

---
+++ src/main/java/com/peng/springbootwar/restcontroller/HelloController.java
@@ -0,0 +1,21 @@
+package com.peng.springbootwar.restcontroller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HelloController {
+    private Logger logger=LoggerFactory.getLogger(HelloController.class);
+
+    @RequestMapping("index")
+    public String index(ModelMap map) {
+        map.put("title","index欢迎页面");
+        logger.info("index页面");
+        return "index";
+    }
+
+
+}
---
+++ src/main/webapp/templates/index.jsp
@@ -0,0 +1,13 @@
+<%@taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page language="java" contentType="text/html; charset=utf-8"
+         pageEncoding="utf-8"%>
+
+<html>
+<head>
+    <title>${title}</title>
+</head>
+<body>
+    这是一个测试页面
+</body>
+</html>
